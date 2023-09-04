8cd7e7b134935833026c9b3e8a4748acdec035b7
sudhir <sudhirvetri@gmail.com>
2023-09-04 08:48:36 +0530

    Create Problem.js

---
+++ ContextAPI/Problem.js
@@ -0,0 +1,34 @@
+// import React from 'react'
+// import { useState } from 'react';
+
+
+// const GrandChild =(props)=>{
+//   return(
+//     <div>
+//       <h3>Grand child</h3>
+//       <Child name={props.name}  />
+//     </div>
+//   )
+// }
+
+// const Child=(props)=>(
+//   <div>
+//     <h2>child :{props.name}</h2>
+    
+//   </div>
+// )
+
+// const App=()=>{
+
+//   const [brand]=useState("Amazon");
+
+//   return (
+//     <div>
+//       <h1>        hello      </h1>
+//       <Child name={brand} />
+//       <GrandChild name={brand} />
+//     </div>
+//   );
+// }
+
+// export default App;
