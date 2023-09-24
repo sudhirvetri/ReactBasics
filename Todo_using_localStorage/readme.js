cadb79eade2f833f5d25e015986ded7d129ee257
sudhir <sudhirvetri@gmail.com>
2023-09-24 12:11:55 +0530

    Create readme.js

---
+++ Todo_using_localStorage/readme.js
@@ -0,0 +1,4 @@
+#Install dependenices first while building the project
+npm install uuid reactstrap bootstrap react-icons
+
+# this code contains app.js for both local storage and session storage , rename the files to app.js while using in your local
